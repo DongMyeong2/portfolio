76f5bc2a0a9266a6bbf68afb89734a6e5d704b4a
DongMyeong2 <144209615+DongMyeong2@users.noreply.github.com>
2025-09-04 12:58:34 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,16 @@
+N = int(input())
+
+T = [0] * N
+P = [0] * N
+
+for i in range(N):
+  T[i], P[i] = map(int, input().split())
+
+dp = [0] * (N+1)
+
+for n in range(N-1, -1, -1):
+  if n+T[n] <= N:
+    dp[n] = max(dp[n+T[n]]+P[n], dp[n+1])
+  else:
+    dp[n] = dp[n+1]
+print(dp[0])
