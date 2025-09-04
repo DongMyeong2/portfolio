834ba04806fa3441e221786c24c2c5df94ca3a18
DongMyeong2 <144209615+DongMyeong2@users.noreply.github.com>
2025-09-04 11:58:54 +0900

    [Silver IV] Title: 수 찾기, Time: 140 ms, Memory: 50396 KB -BaekjoonHub

---
+++ Python/백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,13 @@
+N = int(input())
+N_num = set(map(int, input().split()))  # 리스트를 set으로 변환하여 빠르게 탐색
+M = int(input())
+M_num = list(map(int, input().split()))
+
+result = []
+for m in M_num:
+    if m in N_num:
+        result.append(1)
+    else:
+        result.append(0)
+
+print("\n".join(map(str, result)))
